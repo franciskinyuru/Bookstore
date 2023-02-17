f23a0571b3411fd0c62c1566bcd7bc18e928d1b0
francis kinyuru <franciskinyuru26@gmail.com>
2023-02-17 17:27:20 +0300

    update router

--- routes/BookStore.js
+++ routes/BookStore.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const {getAllBooks,addBook,updateBook,deleteBook} = require('../controllers/BookStore')
 router.post("/bookstore",addBook)
 router.get("/bookstore",getAllBooks)
-router.delete("/bookstore",deleteBook)
+router.delete("/bookstore/:id",deleteBook)
 router.patch("/bookstore/:id",updateBook)
 module.exports = router
