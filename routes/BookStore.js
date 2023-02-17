991c3911efd9811cf3872610ee999fde9bb1a2cc
Esthernjoki <114407843+Esthernjoki@users.noreply.github.com>
2023-02-17 16:46:59 +0300

    updated routes

--- routes/BookStore.js
+++ routes/BookStore.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {getAllBooks,addBook} = require('../controllers/BookStore')
 
-router.route('/bookstore').get(getAllBooks).post(addBook)
-
+//router.route('/bookstore').get(getAllBooks).post(addBook)
+router.post("/bookstore",addBook)
+router.get("/bookstore",getAllBooks)
 module.exports = router
